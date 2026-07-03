0a733c3511d7b7c016bd1a6f7d3e0d839975d40f
max <106026120+maxxfuu@users.noreply.github.com>
2026-07-03 09:52:28 -0700

    Add: is-anagram - submission-23

---
+++ Data Structures & Algorithms/is-anagram/submission-23.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1 = defaultdict(int)
+        map2 = defaultdict(int)
+
+        for char in s:
+            map1[char] += 1
+
+        for char in t:
+            map2[char] += 1
+
+        print(map1, map2)
+        return map1 == map2
+
